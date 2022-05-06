9e5ac733326ada2726331706338eb9286b9e9ea4
cauefurui <cauefurui@gmail.com>
2022-05-06 16:52:22 -0300

    feat: adding email service with nodemailer and mailtrap

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
 import express from 'express'
+import nodemailer from 'nodemailer'
 import { prisma } from './prisma'
 
 const app = express()
 
 app.use(express.json())
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: process.env.MAILTRAP_USER_CODE,
+    pass: process.env.MAILTRAP_USER_PASS
+  }
+});
 
 app.post('/feedbacks', async (req, res) => {
   const { type, comment, screenshot } = req.body
@@ -14,6 +24,18 @@ app.post('/feedbacks', async (req, res) => {
       comment,
       screenshot,
     }
+  })
+
+  await transport.sendMail({ 
+    from: 'Equipe Feedget <oi@feedget.com>',
+    to: 'Caue Furui <cauefurui@gmail.com>',
+    subject: 'Novo feedback',
+    html: [
+      `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
+      `<p>Tipo do feedback: ${type}</p>`,
+      `<p>Tipo do comentário: ${comment}</p>`,
+      `</div>`,
+    ].join('\n')
   })
 
   console.log({ feedback })
